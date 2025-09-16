09efe8ac1225267251449d8000814c9e5d47c29b
Edgar <yeai22@live.com>
2025-09-16 18:18:13 -0500

    Read File

--- main.py
+++ main.py
@@ -4,3 +4,7 @@ import pandas as pd
 import os
 import matplotlib
 
+#Read and show length of file
+data_set = pd.read_csv(r"C:\Users\yeai2_6rsknlh\Downloads\D598 Data Set.csv") # Path to read file
+print("File Read")
+print(f"Length of Data Set is {len(data_set)}") #Show length of data set

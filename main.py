a5a67726992767f4034c026824a8d821598b1a61
Edgar <yeai22@live.com>
2025-09-16 18:20:18 -0500

    Change to Data instead of data_set

--- main.py
+++ main.py
@@ -5,6 +5,9 @@ import os
 import matplotlib
 
 #Read and show length of file
-data_set = pd.read_csv(r"C:\Users\yeai2_6rsknlh\Downloads\D598 Data Set.csv") # Path to read file
+data = pd.read_csv(r"C:\Users\yeai2_6rsknlh\Downloads\D598 Data Set.csv") # Path to read file
 print("File Read")
-print(f"Length of Data Set is {len(data_set)}") #Show length of data set
+print(f"Length of Data Set is {len(data)}") #Show length of data set
+
+#Look for duplicates
+dup_rows = data

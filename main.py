87e5acb0a507eb92f9a9c96b7ff64b5711aa9364
Edgar <yeai22@live.com>
2025-09-16 19:07:26 -0500

    Negative Debt to Equity

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import pandas as pd
 import os
 import matplotlib
 
-#User choice to start reading the file
+#User choice to start Reading the File
 print("Type Ready to Start the Reading of the Data file")
 user_choice = input("")
 
@@ -15,7 +15,7 @@ if user_choice == "Ready":
     print(f"Length of Data Set is {len(data)}") #Show length of data set
 
 #User choice to start Looking for duplicates
-print("Type Ready to Start Looking for duplicates ")
+print("\nType Ready to Start Looking for duplicates ")
 user_choice = input("")
 
 if user_choice == "Ready":
@@ -33,7 +33,7 @@ if user_choice == "Ready":
         print("There are no duplicate rows or ID's")
 
 #User choice to start Grouping
-print("Type Ready to Start Grouping")
+print("\nType Ready to Start Grouping")
 
 user_choice = input("")
 
@@ -42,4 +42,21 @@ if user_choice == "Ready":
     # Group by State and Get Stats
     num_col = data.select_dtypes(include = 'number').columns #Looks for columns with number as values for calulation
     state_stats = data.groupby("Business State")[num_col].agg(["mean", "median", "min","max"]) # Group by Business State and show states
-    print(state_stats.head())
+    print(state_stats.head()) # Show first 5 rows
+
+#User choice to start Filtering Negative Debt
+print("\nType Ready to Start Grouping")
+
+user_choice = input("")
+
+if user_choice == "Ready":
+
+    # Check to see if Debt to Equity is less than zero fro debt
+    negative_data = data[data["Debt to Equity"] < 0] 
+
+    #If statment for negative Debt-to-Equity ratios
+    if len(negative_data) > 0:
+        print(f"There is {len(negative_data)} companies with debt negative Debt-to-Equity ratios")
+        print(negative_data[['Business ID', 'Business State', 'Debt to Equity']].to_string(index=False)) #Selects columns with negative debt and formats for printing
+    else:
+        print("No companies with with debt negative Debt-to-Equity ratios")
